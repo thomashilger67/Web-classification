f219c1900e10ef19a20a55d5f34d3242357287a1
Alexandre <alexandre.bidon.44@gmail.com>
2023-02-23 15:29:50 +0100

    up

---
+++ url_categorization_tensorflow/test.py
@@ -0,0 +1,3 @@
+# Voir https://www.holisticseo.digital/python-seo/website-categorization/
+# Utilise TensorFlow pour encoder les titres et les descriptions des pages
+# Comparer les performances de cette méthode avec l'autre méthode
