67c17ff313039cca5a45d88c1691e70685c6d6c5
Alexandre <alexandre.bidon.44@gmail.com>
2023-03-17 15:13:01 +0100

    up

--- README.md
+++ README.md
@@ -7,10 +7,14 @@ Github repository for the end-of-the-year project with IMATAG at ENSAI. The goal
 You can try to categorize a website using the following command :
 
 ```cmd
-python3 main.py --website_url <<insert_an_url_here>>
+python3 main.py --website_url <<insert_an_url_here>> --predict_proba <<True or False>>
 ```
 
-This command will return the predicted category for the website. You can also select the model you want to use using the `--model` parameter (not working yet).
+This command will return the predicted category for the website. 
+
+If `--predict_proba` is set to False it only return the category predicted. If it is set to `True` it returns the probability for all the categories.
+
+You can also select the model you want to use using the `--model` parameter (not working yet).
 
 Here are some examples (with a naive bayesian model) :
 
--- predict_new_site/main.py
+++ predict_new_site/main.py
@@ -6,16 +6,22 @@ import pandas as pd
 
 @click.command()
 @click.option('--website_url', default="https://ensai.fr", help='The website to categorize.')
-def predict_category(website_url):
+@click.option('--predict_proba', default="False", help='')
+def predict_category(website_url,predict_proba):
     category = ['Arts', 'Business', 'Computers', 'Games', 'Health', 'Home', 'News',
        'Recreation', 'Reference', 'Science', 'Shopping', 'Society',
        'Sports']
     desc = scrap_data_from_website(website_url)
     loaded_model = joblib.load('model_pipeline/pipeline_nb.pkl')
     df = pd.DataFrame([desc], columns=['desc'])
-    category_predicted_int = loaded_model.predict(df.desc.values)
-    category_predicted = category[category_predicted_int[0]]
-    print(category_predicted)
+    if predict_proba:
+        category_predicted_int = loaded_model.predict_proba(df.desc.values)
+        result_dict = dict(zip(category, category_predicted_int[0]))
+        print(result_dict)
+    else :
+        category_predicted_int = loaded_model.predict(df.desc.values)
+        category_predicted = category[category_predicted_int[0]]
+        print(category_predicted)
 
 if __name__ == "__main__":
     predict_category()
